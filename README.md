38202aefa7cf32550c55db3eba7ef88b8f746d25
Andres <andres.aguilarfl@gmail.com>
2023-03-21 19:46:07 -0600

    update
    signin and profile creation working

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SocialMedia
  
+Django Project 
+Social media app
+project is social_book
+app is core
--- social_book/core/admin.py
+++ social_book/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+#to manage profile on the django admin 
+admin.site.register(Profile)
